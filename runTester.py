1d2363316f8a83382ec67cbcdc26d3dc8e9ced71
David DeFazio <davedefazio1@gmail.com>
2019-09-13 11:01:14 -0400

    Fixed auto grader, uploaded lab2 script

--- runTester.py
+++ runTester.py
@@ -1,9 +1,9 @@
 import os
 import csv
 
-assignment_name = "lab-01"
-package_name = "lab01"
-tester_file_path = "/home/mllab/Desktop/CS140/Grading_Scripts/Testers/Labs/Lab01_Grader.java"
+assignment_name = "lab-02"
+package_name = "lab02"
+tester_file_path = "/home/mllab/Desktop/CS140/Grading_Scripts/Testers/Labs/Lab02_Grader.java"
 
 path_to_submissions = "/home/mllab/Desktop/CS140/Grading_Scripts/" + assignment_name + '/'
 STUDENT_INFO_FILE = "studentGithubUsernames.csv"
@@ -36,27 +36,53 @@ print("Grading", len(student_folders), "Assignments")
 #Map student id to github id
 id_to_github, id_to_name = get_student_info()
 
-#Make report.txt file
-f = open(path_to_submissions+"report.txt",'w')
-f.close()
 
 #Run tester for each folder
 for student in student_folders:
 
-	#Append student name to report.txt
-	f = open(path_to_submissions+"report.txt",'a')
-	f.write('\n\n' + id_to_name[student] + '\n')
-	f.close()
+	if(student != 'report.txt'):
 
-	#Get path to particular students code
-	path_to_code = path_to_submissions+student+'/'+assignment_name+'-'+id_to_github[student]+'/'
+		f = open(path_to_submissions+"report.txt",'a')
 
-	#Copy tester file to students code
-	os.system("cp " + tester_file_path + ' ' + path_to_code)
+		#Append student name to report.txt
+		f.write('\n\n' + id_to_name[student] + '\n')
 
-	#compile (including tester)
-	os.system("javac -d . "+path_to_code+"*.java")
+		#Get path to particular students code
+		path_to_code = path_to_submissions+student+'/'+assignment_name+'-'+id_to_github[student]+'/'
 
-	#Run tester. The tester appends the grade and errors to report.txt
-	os.system("java "+package_name+'.'+tester_file_name)
+		print(path_to_code)
+		#Copy tester file to students code
+		call1 = os.system("cp " + tester_file_path + ' ' + path_to_code)
+
+		if(call1 != 0):
+			f.write("Failed to copy over tester, this message should never appear!!\n")
+
+		#compile (including tester)
+		call2 = os.system("javac -d . "+path_to_code+"*.java")
+
+		if(call2 != 0):
+			f.write("FAILED TO COMPILE. IGNORE GIVEN GRADE AND CHECK MANUALLY\n")
+			f.close()
+		else:
+
+			#Let grader take file during runtime
+			f.close()
+
+			#Run tester. The tester appends the grade and errors to report.txt
+			#We don't need to point to students folder to run, as .class files
+			#are saved directly to Grading_Scripts/
+			call3 = os.system("java "+package_name+'.'+tester_file_name)
+
+			if(call3 != 0):
+				f = open(path_to_submissions+"report.txt",'a')
+				f.write("RUNTIME ERROR!! Check manually unless the tester catches this\n")
+				f.close()
+
+			#Now remove .class files to ensure next student is fresh compile
+			call4 = os.system("rm -r " + package_name + '/')
+			if(call4 != 0):
+				f = open(path_to_submissions+"report.txt",'a')
+				f.write("Failed to remove .class files. This should never appear.\n")
+				f.close()
+
 
