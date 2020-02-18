aeb2813aebab4585979b18a75b27c2b892f09c8e
Gregory Wint <gregwint95@gmail.com>
2020-02-18 16:50:27 -0500

    Moved old testers

---
+++ Testers/Assignments/Assignment02Tests.java
@@ -0,0 +1,60 @@
+package assignment02;
+
+import org.junit.*;
+import gradingplatform.Instance;
+import gradingplatform.ClassWrapper;
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+
+public class Assignment02Tests {
+    ClassWrapper studentWrapper;
+
+    @Before
+    public void init() throws Exception {
+    }
+
+    @Test
+    public void testMostPages() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testNumPagesInLibrary() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeStringArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testTriangleArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeSymmetricArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakePascalTriangle() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeRowMajor() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testUnfoldRowMajor() {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testSortByGPA() throws Exception {
+        Assert.assertTrue(false);
+    }
+}
