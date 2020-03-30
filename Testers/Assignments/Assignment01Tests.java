c432a8e9ae5a9c5f2309496bfadb3b501744f27a
Gregory Wint <gregwint95@gmail.com>
2020-03-29 21:38:21 -0400

    Fix error in runtester

---
+++ Testers/Assignments/Assignment01Tests.java
@@ -0,0 +1,61 @@
+package assignment01;
+
+import org.junit.*;
+import wrapper.Instance;
+import wrapper.ClassWrapper;
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class Assignment01Tests {
+
+    @Test
+    public void testMostPages() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testNumPagesInLibrary() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeStringArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testTriangleArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeSymmetricArray() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakePascalTriangle() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeRowMajor() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMakeRowMajorNull() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testUnfoldRowMajor() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testSortByGPA() throws Exception {
+        Assert.assertTrue(false);
+    }
+}
